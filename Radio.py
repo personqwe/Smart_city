fb423b818b562f59cc0be58802bee024d738e308
Kyungrok <kllee@huconn.com>
2023-08-29 09:16:26 +0900

    [Smart_city] - 버튼을 통해 다음 노래를 송신하도록 수정

--- Radio.py
+++ Radio.py
@@ -16,6 +16,8 @@ def on_disconnect():
 
 # GPIO 핀 번호 설정
 button_pin = 90  # 버튼의 GPIO 핀 번호
+musicFolder = '/root/MagicMirror/Music/'  # 음악 파일 경로
+musicFiles = []  # 음악 파일 리스트를 저장할 리스트 변수
 
 # GPIO 핀 모드 설정
 def setup_gpio(pin):
@@ -34,7 +36,27 @@ def Play_Radio():
 
 def Stop_Radio():
     sio.emit('radio', 'stop_radio')
-    print('Button pressed again! Sending stop_radio')
+    print('Button pressed! Sending stop_radio')
+
+def Play_Music():
+    sio.emit('Play_Music', 'Play_Music')
+    print('Button pressed! Sending Play_Music')
+
+def Next_Music():
+    sio.emit('Next_Music', 'Next_Music')
+    print('Button pressed! Sending Play_Music')
+
+def File_Load():
+    global musicFiles  # musicFiles 변수를 함수 내에서 수정 가능하도록 global 선언
+    try:
+        files = os.listdir(musicFolder)
+        musicFiles = [file for file in files if file.endswith('.mp3')]
+        print('파일 불러옴', musicFiles)
+        if len(musicFiles) == 0:
+            print('디렉토리에서 음악 파일을 찾을 수 없습니다.')
+            return
+    except Exception as err:
+        print(f'디렉토리 읽기 오류: {err}')
 
 # GPIO 설정 및 버튼 이벤트 처리
 def setup_and_poll_button(pin):
@@ -48,6 +70,10 @@ def setup_and_poll_button(pin):
             if button_press_count == 1:
                 Play_Radio()
             elif button_press_count == 2:
+                Play_Music()
+            elif button_press_count < len(musicFiles) + 2:
+                Next_Music()
+            elif button_press_count == len(musicFiles) + 2:
                 Stop_Radio()
                 button_press_count = 0  # 초기화
         prev_button_state = button_state
@@ -55,6 +81,8 @@ def setup_and_poll_button(pin):
 
 if __name__ == "__main__":
     try:
+        File_Load()  # 음악 파일 불러오기
+        num_music_files = len(musicFiles)  # 음악 파일 갯수 저장
         button_thread = threading.Thread(target=setup_and_poll_button, args=(button_pin,))
         button_thread.start()
 
